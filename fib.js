b91ec42eda69bcfb46963520e1b3a2837611eba3
MichaelShawUK <MichaelShawUK@gmail.com>
2022-11-19 17:57:07 +0000

    Output Fibonacci sequence iteratively or recursively

---
+++ fib.js
@@ -0,0 +1,20 @@
+function fibonacciIterative(n) {
+  let result = [0, 1];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+  for (let i = 3; i <= n; i++) {
+    result.push(result[result.length - 1] + result[result.length - 2]);
+  }
+  return result;
+}
+
+function fibonacciRecursive(n) {
+  let result = [0, 1];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+  else {
+    result = fibsRec(n - 1);
+    result.push(result[n - 3] + result[n - 2]);
+    return result;
+  }
+}
